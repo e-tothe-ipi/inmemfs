0896c947be0298059d08438a52f487efd2402e67
e-tothe-ipi <bercovitz@gmail.com>
2015-10-27 23:05:53 -0700

    implemented enough to touch a file

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"sync"
 	"time"
+	"syscall"
 
 	"github.com/hanwen/go-fuse/fuse"
 	"github.com/hanwen/go-fuse/fuse/nodefs"
@@ -37,6 +39,7 @@ func newInMemFS() *inMemFS {
 	out := &inMemFS{}
 	out.root = out.createNode()
 	out.root.attr.Mode = fuse.S_IFDIR | 0755
+	out.root.attr.Nlink = 2
 	return out
 }
 
@@ -48,13 +51,27 @@ func (fs *inMemFS) createNode() *inMemNode {
 	node := &inMemNode{fs: fs,}
 	now := time.Now()
 	node.attr.SetTimes(&now, &now, &now)
+	node.attr.Nlink = 1
 	return node
 }
 
 type inMemNode struct {
 	fs *inMemFS
 	inode *nodefs.Inode
+	metadataMutex sync.RWMutex
 	attr fuse.Attr
+}
+
+func (node *inMemNode) incrementLinks() {
+	node.metadataMutex.Lock()
+	node.attr.Nlink += 1
+	node.metadataMutex.Unlock()
+}
+
+func (node *inMemNode) decrementLinks() {
+	node.metadataMutex.Lock()
+	node.attr.Nlink -= 1
+	node.metadataMutex.Unlock()
 }
 
 func (node *inMemNode) Inode() *nodefs.Inode {
@@ -66,18 +83,20 @@ func (node *inMemNode) SetInode(inode *nodefs.Inode) {
 }
 
 func (node *inMemNode) OnMount(conn *nodefs.FileSystemConnector) {
-
+	fmt.Printf("Mounted\n")
 }
 
 func (node *inMemNode) OnUnmount() {
-
+	fmt.Printf("Unmounted\n")
 }
 
 func (parent *inMemNode) Lookup(out *fuse.Attr, name string, context *fuse.Context) (*nodefs.Inode, fuse.Status) {
 	child := parent.inode.GetChild(name)
 	if child != nil {
 		if inMemChild, success := child.Node().(*inMemNode); success {
+			inMemChild.metadataMutex.RLock()
 			*out = inMemChild.attr
+			inMemChild.metadataMutex.RUnlock()
 		}
 		return child, fuse.OK
 	}
@@ -107,32 +126,73 @@ func (node *inMemNode) Mknod(name string, mode uint32, dev uint32, context *fuse
 func (parent *inMemNode) Mkdir(name string, mode uint32, context *fuse.Context) (newNode *nodefs.Inode, code fuse.Status) {
 	node := parent.fs.createNode()
 	node.attr.Mode = mode | fuse.S_IFDIR
+	node.attr.Nlink = 2
 	inode := parent.inode.NewChild(name, true, node)
+	parent.incrementLinks()
 	return inode, fuse.OK
 }
 
 func (node *inMemNode) Unlink(name string, context *fuse.Context) (code fuse.Status) {
-	return fuse.ENOSYS
+	child := node.Inode().GetChild(name)
+	if(child == nil) {
+		return fuse.ENOENT
+	}
+	node.Inode().RmChild(name)
+	if inMemChild, ok := child.Node().(*inMemNode); ok {
+		inMemChild.decrementLinks()
+		inMemChild.metadataMutex.RLock()
+		if inMemChild.attr.Nlink == 0 {
+			node.decrementLinks()
+		}
+		inMemChild.metadataMutex.RUnlock()
+	}
+	return fuse.OK
 }
 
 func (node *inMemNode) Rmdir(name string, context *fuse.Context) (code fuse.Status) {
-	return fuse.ENOSYS
+	return node.Unlink(name, context)
 }
 
 func (node *inMemNode) Symlink(name string, content string, context *fuse.Context) (*nodefs.Inode, fuse.Status) {
 	return nil, fuse.ENOSYS
 }
 
-func (node *inMemNode) Rename(oldName string, newParent nodefs.Node, newName string, context *fuse.Context) (code fuse.Status) {
-	return fuse.ENOSYS
+func (parent *inMemNode) Rename(oldName string, newParent nodefs.Node, newName string, context *fuse.Context) (code fuse.Status) {
+	child := parent.Inode().GetChild(oldName)
+	if(child == nil) {
+		return fuse.ENOENT
+	}
+	parent.Inode().RmChild(oldName)
+	parent.decrementLinks()
+	newParent.Inode().RmChild(newName)
+	newParent.Inode().AddChild(newName, child)
+	if inMemNewParent, ok := newParent.(*inMemNode); ok {
+		inMemNewParent.incrementLinks()
+	}
+	return fuse.OK
 }
 
 func (node *inMemNode) Link(name string, existing nodefs.Node, context *fuse.Context) (newNode *nodefs.Inode, code fuse.Status) {
-	return nil, fuse.ENOSYS
+	if node.Inode().GetChild(name) != nil {
+		return nil, fuse.Status(syscall.EEXIST)
+	}
+	node.Inode().AddChild(name, existing.Inode())
+	if inMemChild, ok := existing.(*inMemNode); ok {
+		inMemChild.incrementLinks()
+	}
+	return existing.Inode(), fuse.OK
 }
 
-func (node *inMemNode) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, child *nodefs.Inode, code fuse.Status) {
-	return nil, nil, fuse.ENOSYS
+func (parent *inMemNode) Create(name string, flags uint32, mode uint32, context *fuse.Context) (file nodefs.File, child *nodefs.Inode, code fuse.Status) {
+	if parent.Inode().GetChild(name) != nil {
+		return nil, nil, fuse.Status(syscall.EEXIST)
+	}
+	node := parent.fs.createNode()
+	node.attr.Mode = mode | fuse.S_IFREG
+	parent.Inode().NewChild(name, false, node)
+	parent.incrementLinks()
+	f := node.createFile()
+	return f, node.Inode(), fuse.OK
 }
 
 func (node *inMemNode) Open(flags uint32, context *fuse.Context) (file nodefs.File, code fuse.Status) {
@@ -143,8 +203,10 @@ func (node *inMemNode) OpenDir(context *fuse.Context) ([]fuse.DirEntry, fuse.Sta
 	children := node.inode.FsChildren()
 	ls := make([]fuse.DirEntry, 0, len(children))
 	for name, inode := range children {
-		if childNode, err := inode.Node().(*inMemNode); err {
+		if childNode, success := inode.Node().(*inMemNode); success {
+			childNode.metadataMutex.RLock()
 			ls = append(ls, fuse.DirEntry{Name: name, Mode: childNode.attr.Mode})
+			childNode.metadataMutex.RUnlock()
 		}
 	}
 	return ls, fuse.OK
@@ -175,7 +237,9 @@ func (node *inMemNode) ListXAttr(context *fuse.Context) (attrs []string, code fu
 }
 
 func (node *inMemNode) GetAttr(out *fuse.Attr, file nodefs.File, context *fuse.Context) (code fuse.Status) {
+	node.metadataMutex.RLock()
 	*out = node.attr
+	node.metadataMutex.RUnlock()
 	return fuse.OK
 }
 
@@ -192,7 +256,11 @@ func (node *inMemNode) Truncate(file nodefs.File, size uint64, context *fuse.Con
 }
 
 func (node *inMemNode) Utimens(file nodefs.File, atime *time.Time, mtime *time.Time, context *fuse.Context) (code fuse.Status) {
-	return fuse.ENOSYS
+	node.metadataMutex.Lock()
+	changeTime := node.attr.ChangeTime()
+	node.attr.SetTimes(atime, mtime, &changeTime)
+	node.metadataMutex.Unlock()
+	return fuse.OK
 }
 
 func (node *inMemNode) Fallocate(file nodefs.File, off uint64, size uint64, mode uint32, context *fuse.Context) (code fuse.Status) {
@@ -201,6 +269,87 @@ func (node *inMemNode) Fallocate(file nodefs.File, off uint64, size uint64, mode
 
 func (node *inMemNode) StatFs() *fuse.StatfsOut {
 	return &fuse.StatfsOut{}
+}
+
+var _ nodefs.File = (*inMemFile)(nil)
+
+type inMemFile struct {
+	node *inMemNode
+}
+
+func (node *inMemNode) createFile() *inMemFile {
+	return &inMemFile{node: node}
+}
+
+// Called upon registering the filehandle in the inode.
+func (f *inMemFile) SetInode(inode *nodefs.Inode) {
+	if f.node.inode != inode {
+		panic("inMemFile: wrong inode detected")
+	}
+}
+
+// The String method is for debug printing.
+func (f *inMemFile) String() string {
+	return fmt.Sprintf("inMemFile")
+}
+
+// Wrappers around other File implementations, should return
+// the inner file here.
+func (f *inMemFile) InnerFile() nodefs.File {
+	return nil
+}
+
+func (f *inMemFile) Read(dest []byte, off int64) (fuse.ReadResult, fuse.Status){
+	return nil, fuse.ENOSYS
+}
+
+func (f *inMemFile) Write(data []byte, off int64) (written uint32, code fuse.Status) {
+	return uint32(len(data)), fuse.OK
+}
+
+// Flush is called for close() call on a file descriptor. In
+// case of duplicated descriptor, it may be called more than
+// once for a file.
+func (f *inMemFile) Flush() fuse.Status {
+	return fuse.OK
+}
+
+// This is called to before the file handle is forgotten. This
+// method has no return value, so nothing can synchronizes on
+// the call. Any cleanup that requires specific synchronization or
+// could fail with I/O errors should happen in Flush instead.
+func (f *inMemFile) Release() {
+
+}
+
+func (f *inMemFile) Fsync(flags int) (code fuse.Status) {
+	return fuse.OK
+}
+
+// The methods below may be called on closed files, due to
+// concurrency.  In that case, you should return EBADF.
+func (f *inMemFile) Truncate(size uint64) fuse.Status {
+	return fuse.ENOSYS
+}
+
+func (f *inMemFile) GetAttr(out *fuse.Attr) fuse.Status {
+	return f.node.GetAttr(out, f, nil)
+}
+
+func (f *inMemFile) Chown(uid uint32, gid uint32) fuse.Status {
+	return f.node.Chown(f, uid, gid, nil)
+}
+
+func (f *inMemFile) Chmod(perms uint32) fuse.Status {
+	return f.node.Chmod(f, perms, nil)
+}
+
+func (f *inMemFile) Utimens(atime *time.Time, mtime *time.Time) fuse.Status {
+	return f.node.Utimens(f, atime, mtime, nil)
+}
+
+func (f *inMemFile) Allocate(off uint64, size uint64, mode uint32) (code fuse.Status) {
+	return f.node.Fallocate(f, off, size, mode, nil)
 }
 
 
